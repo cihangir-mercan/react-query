8610f19c330bf0d1f7ad3ae76daac237cb194063
cihangir-mercan <g5837441b>
2022-04-10 03:25:10 +0300

    Translate without submit button

--- src/components/AddText.js
+++ src/components/AddText.js
@@ -1,15 +1,17 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {useMutation, useQueryClient} from "react-query";
+import {useDebounce} from "../customHooks";
 import {addText} from "../apis";
 import AddVoiceText from "./AddVoiceText";
 import {autoResize, isBlank, translateHelper} from "../utils";
-import {Button} from "@blueprintjs/core";
+import {Intent, Spinner} from "@blueprintjs/core";
 import "./AddText.scss";
 
 const AddText = () => {
     const leftTextArea = useRef(null);
     const rightTextArea = useRef(null);
     const [text, setText] = useState("");
+    const debouncedText = useDebounce(text, 500);
     const [translation, setTranslation] = useState("");
     const [translationLoading, setTranslationLoading] = useState(false);
 
@@ -22,32 +24,33 @@ const AddText = () => {
         },
     });
 
+    // Only call effect if debounced search term changes
     useEffect(() => {
+        if (isBlank(debouncedText)) {
+            return;
+        }
+
         // auto resize textareas at text change if height becomes too long
         if (leftTextArea && rightTextArea) {
             autoResize(leftTextArea.current, rightTextArea.current);
         }
-    }, [text])
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+        setTranslationLoading(true);
 
-        if (isBlank(text)) {
-            return;
-        }
-
-        setTranslationLoading(true)
-        translateHelper(text).then(translatedText => {
+        translateHelper(debouncedText).then(translatedText => {
             setTranslation(translatedText)
 
-            mutation.mutate({text, translatedText});
+            mutation.mutate({
+                text: debouncedText,
+                translatedText: translatedText
+            });
 
             setTranslationLoading(false);
         })
-    }
+    }, [debouncedText]);
 
     return <div className="translate__wrapper">
-        <form className="translate__form" onSubmit={handleSubmit}>
+        <div className="translate__form">
             <textarea
                 ref={leftTextArea}
                 className="bp4-input bp4-large left__textarea"
@@ -57,12 +60,10 @@ const AddText = () => {
                 value={text}
                 onChange={e => setText(e.target.value)}>
             </textarea>
-            <Button type="submit"
-                    icon="translate"
-                    large
-                    intent="primary"
-                    loading={translationLoading}
-                    aria-label="Translate"/>
+            <Spinner
+                className={(translationLoading ? "visible" : "")}
+                intent={Intent.PRIMARY}
+                size={40} />
             <textarea
                 ref={rightTextArea}
                 className="bp4-input bp4-large right__textarea"
@@ -73,10 +74,9 @@ const AddText = () => {
                 readOnly
                 disabled>
             </textarea>
-        </form>
+        </div>
         <AddVoiceText setText={setText}/>
     </div>
-
 };
 
 export default AddText;
